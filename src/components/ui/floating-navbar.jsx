9703abf3b8426879366899b2a860eb9ec19b51a7
zoro-diablo <vyshnavk117@gmail.com>
2025-01-10 15:28:07 +0530

    Enhance FloatingNav component to hide on scroll for improved user experience

--- src/components/ui/floating-navbar.jsx
+++ src/components/ui/floating-navbar.jsx
@@ -1,10 +1,32 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 import { Link } from "react-router-dom";
 
 export const FloatingNav = ({ className }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // console.log("Scroll position:", window.scrollY); 
+      setIsVisible(window.scrollY <= 1200);
+    };
+
+    
+    window.addEventListener("scroll", handleScroll);
+
+   
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  if (!isVisible) {
+     
+    return null; 
+  }
+
   return (
     <motion.div
       initial={{
